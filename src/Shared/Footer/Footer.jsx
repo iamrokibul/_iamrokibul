811870a5faf02077846e156c9cd1e21c748a3f68
Md. Rokibul Hasan <76747431+iamrokibul@users.noreply.github.com>
2025-04-24 09:30:07 +0400

    Update Footer.jsx
    Updated footer current year

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <div>
-            <p className="text-sm">&copy; Copyright | <Link className="text-[#00d8ff]" to='/'>IamRokibul</Link> | 2024</p>
+            <p className="text-sm">&copy; Copyright | <Link className="text-[#00d8ff]" to='/'>IamRokibul</Link> | 2025</p>
         </div>
     );
 };
